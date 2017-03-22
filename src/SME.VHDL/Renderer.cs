b74f1d3e2c854ac7673c76a6c6adbd60892ab922
Kenneth Skovhede <kenneth@hexad.dk>
2017-03-22 13:35:56 +0100

    Added some of the VHDL components that used to be in the previous VHDL render

---
+++ src/SME.VHDL/Renderer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SME.VHDL
+{
+	/// <summary>
+	/// Static helper to simplify rendering without using a simulation
+	/// </summary>
+	public static class Renderer
+	{
+		/// <summary>
+		/// Performs the VHDL rendering.
+		/// </summary>
+		/// <param name="processes">The processes to parse.</param>
+		/// <param name="targetfolder">The folder where the output is stored.</param>
+		/// <param name="backupfolder">The folder where backups are stored.</param>
+		/// <param name="csvtracename">The name of the CSV trace file.</param>
+		public static void Render(IEnumerable<IProcess> processes, string targetfolder, string backupfolder = null, string csvtracename = null)
+		{
+			new RenderState(processes, targetfolder, backupfolder, csvtracename).Render();
+		}
+	}
+}
