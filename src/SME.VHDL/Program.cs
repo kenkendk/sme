5a6289522736f1462000cb15b030d0c178ec4010
Kenneth Skovhede <kenneth@hexad.dk>
2017-03-30 13:20:26 +0200

    Downgraded CommandLine parser to non-beta

--- src/SME.VHDL/Program.cs
+++ src/SME.VHDL/Program.cs
@@ -8,37 +8,37 @@ namespace SME.VHDL
 	{
 		private class Options
 		{
-			[Value(0, MetaName="assemblypath", MetaValue="DLLPATH", HelpText="The path to the assembly to generate VHDL for")]
+			[ValueOption(0)]
+			[Option(/*0, MetaName="assemblypath",*/ MetaValue="DLLPATH", HelpText="The path to the assembly to generate VHDL for")]
 			public string AssemblyPath { get; set; }
 
-			[Value(1, MetaName="targetfolder", MetaValue="FOLDERPATH", HelpText="The path to the folder where VHDL output is generated")]
+			[ValueOption(1)]
+			[Option(/*1, MetaName="targetfolder",*/ MetaValue="FOLDERPATH", HelpText="The path to the folder where VHDL output is generated")]
 			public string TargetFolder { get; set; }
 
 			[Option(HelpText="Path to a folder where backups of the VHDL files are stored, to avoid loosing information with overwrites")]
 			public string BackupFolder { get; set; }
 
-			[Option(Default="TestData.csv", HelpText="The string to insert into the testbench, indicating what trace file to load")]
+			[Option(DefaultValue="TestData.csv", HelpText="The string to insert into the testbench, indicating what trace file to load")]
 			public string CSVPath { get; set; }
 		}
 
 		public static int Main(string[] args)
 		{
-			var pr = CommandLine.Parser.Default.ParseArguments<Options>(args);
-			return pr.MapResult(
-				options => {
-					if (!File.Exists(options.AssemblyPath))
-					{
-						Console.WriteLine("File not found: {0}", options.AssemblyPath);
-						return 1;
-					}
+			var options = new Options();
 
-					var asm = System.Reflection.Assembly.UnsafeLoadFrom(options.AssemblyPath);
-					new RenderState(Loader.LoadAssemblies(asm), options.TargetFolder, options.BackupFolder, options.CSVPath).Render();			
-					return 0;
-				},
+			if (!CommandLine.Parser.Default.ParseArguments(args, options))
+				return 1;
 
-				error => 1
-			);
+			if (!File.Exists(options.AssemblyPath))
+			{
+				Console.WriteLine("File not found: {0}", options.AssemblyPath);
+				return 1;
+			}
+
+			var asm = System.Reflection.Assembly.UnsafeLoadFrom(options.AssemblyPath);
+			new RenderState(Loader.LoadAssemblies(asm), options.TargetFolder, options.BackupFolder, options.CSVPath).Render();			
+			return 0;
 		}
 	}
 }
