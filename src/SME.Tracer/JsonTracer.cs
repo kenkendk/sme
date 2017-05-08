4fea0f94afac6d3effc4a615439f9e18b69e609b
Kenneth Skovhede <kenneth@hexad.dk>
2017-05-08 13:01:11 +0200

    Fixed the JSON output from the tracer

--- src/SME.Tracer/JsonTracer.cs
+++ src/SME.Tracer/JsonTracer.cs
@@ -23,29 +23,37 @@ namespace SME.Tracer
 
 			m_stream = new StreamWriter(m_filename, false, System.Text.Encoding.UTF8);
 			m_writer = new JsonTextWriter(m_stream);
+
+			m_writer.Formatting = Newtonsoft.Json.Formatting.Indented;
+
 			m_writer.WriteStartObject();
 		}
 
 		protected override void OutputSignalNames(SignalEntry[] signals)
 		{
 			m_writer.WritePropertyName("signals");
-			m_writer.WriteStartObject();
+			m_writer.WriteStartArray();
 
 			foreach (var p in signals)
 			{
-				if (p.IsInternal)
-					m_writer.WritePropertyName(p.Property.DeclaringType.DeclaringType.Name + "." + p.Property.DeclaringType.Name + "." + p.Property.Name);
-				else
-					m_writer.WritePropertyName(p.Property.DeclaringType.Name + "." + p.Property.Name);
 				m_writer.WriteStartObject();
+
+				m_writer.WritePropertyName("name");
+
+				if (p.IsInternal)
+					m_writer.WriteValue(p.Property.DeclaringType.DeclaringType.Name + "." + p.Property.DeclaringType.Name + "." + p.Property.Name);
+				else
+					m_writer.WriteValue(p.Property.DeclaringType.Name + "." + p.Property.Name);
+
 				m_writer.WritePropertyName("driver");
 				m_writer.WriteValue(p.IsDriver);
 
 				m_writer.WriteEndObject();
 			}
+	
+			m_writer.WriteEndArray();
 
-			m_writer.WriteEndObject();
-
+			m_writer.WritePropertyName("values");
 			m_writer.WriteStartArray();
 		}
 
@@ -54,7 +62,14 @@ namespace SME.Tracer
 			m_writer.WriteStartArray();
 
 			foreach (var v in values)
-				m_writer.WriteValue(v.Item2);
+				if (v.Item2 is SME.ReadViolationException)
+					m_writer.WriteValue('U');
+				else if (v.Item2 != null && v.Item2.GetType().IsConstructedGenericType)
+				{
+					m_writer.WriteValue("todo!");
+				}
+				else
+					m_writer.WriteValue(v.Item2);
 
 			m_writer.WriteEndArray();
 		}
