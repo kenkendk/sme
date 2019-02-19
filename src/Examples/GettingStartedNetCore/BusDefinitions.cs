58dd7c5246a36f95aad272d3f575189a862d22e7
Kenneth Skovhede <kenneth@hexad.dk>
2019-02-19 15:26:29 +0100

    Added a `GettingStarted` example for .Net core with VS Code

---
+++ src/Examples/GettingStartedNetCore/BusDefinitions.cs
@@ -0,0 +1,69 @@
+﻿using SME;
+
+namespace GettingStarted
+{
+	/// <summary>
+	/// A bus for reading image values from a sensor,
+	/// one pixel at a time
+	/// </summary>
+	public interface ImageInputLine : IBus
+	{
+        // The [InitialValue] attribute makes sure we can read the value without writing.
+		// It is also possible to set [InitializedBus] to force all fields to be initialized
+		
+		/// <summary>
+		/// A value sent from the camera indicating if the RGB value is valid
+		/// </summary>
+		[InitialValue]
+		bool IsValid { get; set; }
+
+		/// <summary>
+		/// A value sent from the camera indicating if the current pixel is the last in the image
+		/// </summary>
+		[InitialValue]
+		bool LastPixel { get; set; }
+
+		/// <summary>
+		/// The red component of the pixel
+		/// </summary>
+		byte R { get; set; }
+        /// <summary>
+        /// The gree component of the pixel
+        /// </summary>
+        byte G { get; set; }
+        /// <summary>
+        /// The blue component of the pixel
+        /// </summary>
+        byte B { get; set; }
+	}
+
+	/// <summary>
+	/// Bus for reporting counts in the bins
+	/// </summary>
+	public interface BinCountOutput : IBus
+	{
+		/// <summary>
+		/// A value send by bin counter indicating if the output represents a whole image
+		/// </summary>
+		[InitialValue]
+		bool IsValid { get; set; }
+
+		/// <summary>
+		/// The number of low intensity pixels
+		/// </summary>
+		[InitialValue]
+		uint Low { get; set; }
+
+		/// <summary>
+		/// The number of medium intensity pixels
+		/// </summary>
+		[InitialValue]
+		uint Medium { get; set; }
+
+		/// <summary>
+		/// The number of high intensity pixels
+		/// </summary>
+		[InitialValue]
+		uint High { get; set; }
+	}
+}
---
+++ src/Examples/GettingStartedNetCore/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using SME;
+
+namespace GettingStarted
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+
+            using(var sim = new Simulation())
+            {
+                var simulator = new ImageInputSimulator("image1.png");
+                var calculator = new ColorBinCollector(simulator.Data);
+
+                // Use fluent syntax to configure the simulator.
+                // The order does not matter, but `Run()` must be 
+                // the last method called.
+
+                // The top-level input and outputs are exposed
+                // for interfacing with other VHDL code or board pins
+
+                sim
+                    .AddTopLevelOutputs(calculator.Output)
+                    .AddTopLevelInputs(simulator.Data)
+                    .BuildCSVFile()
+                    .BuildVHDL()
+    			    .Run();
+
+                // After `Run()` has been invoked the folder
+                // `output/vhdl` contains a Makefile that can
+                // be used for testing the generated design
+            }
+		}
+	}
+}
